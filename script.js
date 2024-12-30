7f39acdb42a64c6a8d1e78142efdd838533e3769
polianamadmimic <72580718+polianachagas@users.noreply.github.com>
2024-12-30 19:32:23 -0300

    search bar and final styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $.getJSON(
-    "https://api.openweathermap.org/data/2.5/weather?lat=40&lon=-110&appid=c8b893fffcae5507df084e5a9ff684d0", 
+    "https://api.openweathermap.org/data/2.5/weather?lat=35.7258&lon=139.539&appid=c8b893fffcae5507df084e5a9ff684d0", 
     function(data) {
     console.log(data);
 
@@ -17,46 +17,43 @@ $.getJSON(
     var city = data.name;
     $('.city-country').append(city + ' / ' + country);
 
-    if (data.weather[0].main == 'Clear') { 
-        var icon = 'imgs/clear.png';
-        $('.icon').attr('src', icon);
+    var feels_like = data.main.feels_like;
+    var feels_like_fah = Math.floor((feels_like - 273.15) * 9/5 + 32);
+    var feels_like_c = Math.floor(feels_like - 273.15);
+    $('.feels-like').append(feels_like_fah + 'º / ' + feels_like_c + 'º');
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#6DD5C9";
-    }
-    else if (data.weather[0].main == 'Thunderstrom') {
-        var icon = 'imgs/storm.png';
-        $('.icon').attr('src', icon);
+    var humidity = data.main.humidity;
+    $('.humidity').append(humidity);
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#242038";
-    }
-    else if (data.weather[0].main == 'Drizzle') {
-        var icon = 'imgs/rainy.png';
-        $('.icon').attr('src', icon);
+    var temp_max = data.main.temp_max;
+    var temp_max_fah = Math.floor((temp_max - 273.15) * 9/5 + 32);
+    var temp_max_celsius = Math.floor(temp_max - 273.15);
+    $('.temp-max').append(temp_max_fah + 'º / ' + temp_max_celsius + 'º');
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#64A6BD";
-    }
-    else if (data.weather[0].main == 'Rain') { 
-        var icon = 'imgs/heavy-rain.png';
-        $('.icon').attr('src', icon);
+    var temp_min = data.main.temp_min;
+    var temp_min_fah = Math.floor((temp_min - 273.15) * 9/5 + 32);
+    var temp_min_celsius = Math.floor(temp_min - 273.15);
+    $('.temp-min').append(temp_min_fah + 'º / ' + temp_min_celsius + 'º');
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#26547C";
-    }
-    else if (data.weather[0].main == 'Snow') { 
-        var icon = 'imgs/snow.png';
-        $('.icon').attr('src', icon);
+    document.querySelector('h2').onclick = function() {
+        var moreInfo = document.getElementById('more-info');
+        moreInfo.classList.toggle('open'); // Adiciona ou remove a classe 'open'
+    };
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#DFDFDF";
-    }
-    else if (data.weather[0].main == 'Atmosphere') {
-        var icon = 'imgs/fog.png';
-        $('.icon').attr('src', icon);
+    var weatherConditions = {
+        Clear: {icon: 'imgs/clear.png', color: '#6DD5C9'},
+        Thunderstorm: {icon: 'imgs/storm.png', color: "#242038"},
+        Drizzle: {icon: 'imgs/rainy.png', color: "#64A6BD"},
+        Rain: {icon: 'imgs/heavy-rain.png', color: "#26547C"},
+        Snow: {icon: 'imgs/snow.png', color: "#DFDFDF"},
+        Atmosphere: {icon: 'imgs/fog.png', color: "#E2D0BE"},
+        Clouds: {icon: 'imgs/cloudy.png', color: "#1C57BD"}
+    };
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#E2D0BE";
+    var condition = data.weather[0].main;
+    if (weatherConditions[condition]) {
+        $('.icon').attr('src', weatherConditions[condition].icon);
+        document.getElementsByClassName('container')[0].style.backgroundColor = weatherConditions[condition].color;
     }
-    else if (data.weather[0].main == 'Clouds') { 
-        var icon = 'imgs/cloudy.png';
-        $('.icon').attr('src', icon);
 
-        document.getElementsByClassName('weather-container')[0].style.backgroundColor = "#1C57BD";
-    }
 });
